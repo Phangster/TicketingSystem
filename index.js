a0385b4f248a156b6b7e1be20a74104ebe200a09
yijie <yijie@gigacover.com>
2019-02-15 01:18:10 +0800

    Registration basic case set up

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
 
 const port = process.env.PORT || 5000;
 
+// Config DB
+const db = require('./config/keys').mongoURI;
+
+// Body-parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// Connect to MongoDB via Mongoose
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('/api/users', users);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ routes/api/users.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+
+// Load User model:
+const User = require('../../models/User');
+
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Public
+router.get('/test', (req, res) => res.json({msg: 'Users works!'}));
+
+// @route   POST api/users/register
+// @desc    Register users
+// @access  Public
+router.post('/register', (req, res) => {
+    User.findOne({email:req.body.email}).then(user => {
+        if (user) {
+            return res.status(400).json({email: "Email already exists"});
+
+        } else {
+
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
+
+module.exports = router;
