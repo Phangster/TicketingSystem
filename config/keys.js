72a7f0814236189f56a631333a2eda7ca22ec1ce
yijie <cyberform.jys@gmail.com>
2019-02-21 17:16:38 +0800

    Added login and authentication

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,6 @@ module.exports = {
     // DB for development purposes
     dev: {
         mongoURI: 'mongodb+srv://root:qwerty123@cluster0-jgluf.mongodb.net/test?retryWrites=true'
-    }
+    },
+    secretOrKey: 'secret'
 }
