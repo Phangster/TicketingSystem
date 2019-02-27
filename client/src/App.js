c9a7564de0b164f4bc7b71e760f282d45792fda7
Phangster <mynamebryanphang@gmail.com>
2019-02-27 18:48:33 +0800

    login in and register up

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+// import route Components here
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>hello</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <ul>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            </ul>
+            <hr/>
+            <Switch>
+              <Route path="/" exact={true} component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
-
 export default App;
