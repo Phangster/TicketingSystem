c356c18f1e39f66e3b785a7f36d6f8ab634c6347
Phangster <mynamebryanphang@gmail.com>
2019-03-01 00:35:04 +0800

    contact us

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Register from "./components/Register";
+import Contact from "./components/Contact";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
@@ -7,9 +7,7 @@ import NavBar from "./components/NavBar";
 import {
   BrowserRouter as Router,
   Route,
-  Link,
-  Switch,
-  Redirect
+  Switch
 } from 'react-router-dom'
 class App extends Component {
   render() {
@@ -21,7 +19,7 @@ class App extends Component {
             <Switch>
               <Route path="/" exact={true} component={Home} />
               <Route path="/login" component={Login} />
-              <Route path="/register" component={Register} />
+              <Route path="/contact" component={Contact} />
             </Switch>
           </div>
         </div>
