9b0a41b31f686f959b73fd7ba1b6a8728dbd78dc
Phangster <mynamebryanphang@gmail.com>
2019-03-13 14:00:58 +0800

    added test case

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -88,6 +88,7 @@ class Login extends Component {
             </FormGroup>
           </Col>
           <Button>Submit</Button>
+          <button>hello</button>
       </Form>
       </Container>
     );
